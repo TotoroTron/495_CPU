11451f3d88b269d37f805b3e5b63833b3fbc4eb3
Brian Cheng <brian.cheng.1997@gmail.com>
2019-12-05 16:13:10 -0500

    asdf

--- display.vhd
+++ display.vhd
@@ -14,53 +14,57 @@ end display;
 architecture behavior of display is
 	signal not_hex_0 : std_logic_vector(6 downto 0);
 	signal not_hex_1 : std_logic_vector(6 downto 0);
-
 Begin
-	DISPLAY: process(clk, A_q)
+
+	DISPLAY_PROC: process(clk, A_q)
 	begin
 	if rising_edge(clk) then
 		
 		case A_q(7 downto 4) is
-		when "0000" => hex_1 <= "0111111";
-		When "0001" => hex_1 <= "0000110";
-		When "0010" => hex_1 <= "1011011";
-		When "0011" => hex_1 <= "1001111";
-		When "0100" => hex_1 <= "1100110";
-		When "0101" => hex_1 <= "1101101";
-		When "0110" => hex_1 <= "1111101";
-		When "0111" => hex_1 <= "0000111";
-		When "1000" => hex_1 <= "1111111"; --8
-		When "1001" => hex_1 <= "1101111"; --9
-		When "1010" => hex_1 <= "1110111"; --A
-		When "1011" => hex_1 <= "1111100"; --B
-		when "1100" => hex_1 <= "0111001"; --12
-		when "1101" => hex_1 <= "1011110"; --13
-		when "1110" => hex_1 <= "1111001"; --14
-		when "1111" => hex_1 <= "1110001"; --15
+		when "0000" => not_hex_1 <= "0111111";
+		When "0001" => not_hex_1 <= "0000110";
+		When "0010" => not_hex_1 <= "1011011";
+		When "0011" => not_hex_1 <= "1001111";
+		When "0100" => not_hex_1 <= "1100110";
+		When "0101" => not_hex_1 <= "1101101";
+		When "0110" => not_hex_1 <= "1111101";
+		When "0111" => not_hex_1 <= "0000111";
+		When "1000" => not_hex_1 <= "1111111"; --8
+		When "1001" => not_hex_1 <= "1101111"; --9
+		When "1010" => not_hex_1 <= "1110111"; --A
+		When "1011" => not_hex_1 <= "1111100"; --B
+		when "1100" => not_hex_1 <= "0111001"; --12
+		when "1101" => not_hex_1 <= "1011110"; --13
+		when "1110" => not_hex_1 <= "1111001"; --14
+		when "1111" => not_hex_1 <= "1110001"; --15
+		when others => not_hex_1 <= "0000000";
 		end case;
 		
 		Case A_q(3 downto 0) is
-		when "0000" => hex_0 <= "0111111";
-		When "0001" => hex_0 <= "0000110";
-		When "0010" => hex_0 <= "1011011";
-		When "0011" => hex_0 <= "1001111";
-		When "0100" => hex_0 <= "1100110";
-		When "0101" => hex_0 <= "1101101";
-		When "0110" => hex_0 <= "1111101";
-		When "0111" => hex_0 <= "0000111";
-		When "1000" => hex_0 <= "1111111"; --8
-		When "1001" => hex_0 <= "1101111"; --9
-		When "1010" => hex_0 <= "1110111"; --A
-		When "1011" => hex_0 <= "1111100"; --B
-		when "1100" => hex_0 <= "0111001"; --12
-		when "1101" => hex_0 <= "1011110"; --13
-		when "1110" => hex_0 <= "1111001"; --14
-		when "1111" => hex_0 <= "1110001"; --15
+		when "0000" => not_hex_0 <= "0111111";
+		When "0001" => not_hex_0 <= "0000110";
+		When "0010" => not_hex_0 <= "1011011";
+		When "0011" => not_hex_0 <= "1001111";
+		When "0100" => not_hex_0 <= "1100110";
+		When "0101" => not_hex_0 <= "1101101";
+		When "0110" => not_hex_0 <= "1111101";
+		When "0111" => not_hex_0 <= "0000111";
+		When "1000" => not_hex_0 <= "1111111"; --8
+		When "1001" => not_hex_0 <= "1101111"; --9
+		When "1010" => not_hex_0 <= "1110111"; --A
+		When "1011" => not_hex_0 <= "1111100"; --B
+		when "1100" => not_hex_0 <= "0111001"; --12
+		when "1101" => not_hex_0 <= "1011110"; --13
+		when "1110" => not_hex_0 <= "1111001"; --14
+		when "1111" => not_hex_0 <= "1110001"; --15
+		when others => not_hex_0 <= "0000000";
 		end case;
+	end if;
 		hex_0 <= not not_hex_0;
 		hex_1 <= not not_hex_1;
-	end if;
 	end process;
+	
+
 end behavior;
 
 
--- testbench.vhd
+++ testbench.vhd
@@ -9,24 +9,24 @@ architecture behavioral of testbench is
 		generic(RAM_FILE: string);
 		port(
 			clk_50mhz : in std_logic; --reference clock
-			hex_0 : out std_logic_vector(6 downto 0);
-			hex_1 : out std_logic_vector(6 downto 0);
+			hex0 : out std_logic_vector(6 downto 0);
+			hex1 : out std_logic_vector(6 downto 0);
 						upc_clear : in std_logic
 		);
 	end component;
 	signal clk_50mhz : std_logic;
-	signal hex_0 : std_logic_vector(6 downto 0);
-	signal hex_1 : std_logic_vector(6 downto 0);
+	signal hex0 : std_logic_vector(6 downto 0);
+	signal hex1 : std_logic_vector(6 downto 0);
 	signal upc_clear : std_logic;
 	constant clk_period : time := 20 ns;
 begin
 
 	UUT: top_level
-		GENERIC MAP(RAM_FILE => "ram1.hex")
+		GENERIC MAP(RAM_FILE => "ram3.hex")
 		PORT MAP(
 			clk_50mhz => clk_50mhz,
-			hex_0 => hex_0,
-			hex_1 => hex_1,
+			hex0 => hex0,
+			hex1 => hex1,
 			upc_clear => upc_clear
 		);
 	
